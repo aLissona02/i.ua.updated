8800f5150237b34b2db0b070a74f34c790365729
Alexa <voronych.vl@ukr.net>
2020-01-13 19:19:30 +0200

    version2

--- i.ua.DOM.Framework/i.ua.DOM.Framework/Test.cs
+++ i.ua.DOM.Framework/i.ua.DOM.Framework/Test.cs
@@ -19,46 +19,49 @@ namespace i.ua.DOM.Framework
         }
 
         [Test]
-
         public void testPerform()
         {
-            string login = ConfigurationManager.AppSettings.Get("login");
+            var login = ConfigurationManager.AppSettings.Get("login");
+            var password = ConfigurationManager.AppSettings.Get("pass");
+            var textTo = ConfigurationManager.AppSettings["textTo"];
+            var textMess = ConfigurationManager.AppSettings["textMessage"];
+            var editedFieldTo = ConfigurationManager.AppSettings["textToEdited"];
+            var editedFieldSubject = ConfigurationManager.AppSettings["textSubjectEdited"];
+            var editedFieldText = ConfigurationManager.AppSettings["textMessageEdited"];
+
             LoginPage loginPage = new LoginPage(driver);
+            InputMessagesPage inputMessagesPage = new InputMessagesPage(driver);
+            MessagePage messagePage = new MessagePage(driver);
+            DraftsPage draftsPage = new DraftsPage(driver);
+
             loginPage.EnterLogin(login);
-            string password = ConfigurationManager.AppSettings.Get("pass");
             loginPage.EnterPassword(password);
 
-            InputMessagesPage inputMessagesPage = new InputMessagesPage(driver);
             inputMessagesPage.CreateMessage();
 
-            MessagePage messagePage = new MessagePage(driver);
-            string textTo = ConfigurationManager.AppSettings["textTo"];
             messagePage.FillFieldTo(textTo);
-            string textMess = ConfigurationManager.AppSettings["textMessage"];
             messagePage.FillFieldText(textMess);
             messagePage.SaveMessage();
+           
             inputMessagesPage.ClickOnDraftsButton();
 
-            DraftsPage draftsPage = new DraftsPage(driver);
             draftsPage.ClickOnLastMessage();
-            string editedFieldTo = ConfigurationManager.AppSettings["textToEdited"];
+           
             messagePage.EditFieldTo(editedFieldTo);
-            string editedFieldSubject = ConfigurationManager.AppSettings["textSubjectEdited"];
             messagePage.EditFieldSubject(editedFieldSubject);
-            string editedFieldText = ConfigurationManager.AppSettings["textMessageEdited"];
             messagePage.EditFieldText(editedFieldText);
             messagePage.SaveMessage();
 
             inputMessagesPage.ClickOnDraftsButton();
+
             draftsPage.ClickOnLastMessage();
 
-            string actualFieldTo = messagePage.GetCurrentValueId("to");
+            var actualFieldTo = messagePage.GetCurrentValueId("to");
+            var actualFieldSubject = messagePage.GetCurrentValueName("subject");
+            var actualFieldText = messagePage.GetCurrentValueId("text");
+
             Assert.AreEqual(editedFieldTo, actualFieldTo);
-
-            string actualFieldSubject = messagePage.GetCurrentValueName("subject");
             Assert.AreEqual(editedFieldSubject, actualFieldSubject);
-
-            string actualFieldText = messagePage.GetCurrentValueId("text");
             Assert.AreEqual(editedFieldText, actualFieldText);
 
         }
